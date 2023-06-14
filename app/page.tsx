c28448bb5727a64d5a7b38970dd8fb7c0a71cfc3
malweis <58945171+malweis@users.noreply.github.com>
2023-06-14 00:08:45 -0400

    feat: Añade PostCard
    WE ARE SO BACK AFTER IT WAS SO JOEVER

--- app/page.tsx
+++ app/page.tsx
@@ -1,19 +1,44 @@
+
 import Link from "next/link"
 import { PostWidget, PostCard, Categories } from "@/components"
 import { siteConfig } from "@/config/site"
 import { buttonVariants } from "@/components/ui/button"
+import { getPosts } from '../services';
+interface Post {
+  cursor: string;
 
-const post = [
-  { title: "React Testing", excerpt: "Learn React Testing" },
-  { title: "React with tailwind", excerpt: "Learn React with tailwind" },
-]
+    author: {
+      bio: string;
+      name: string;
+      id: string;
+      photo: {
+        url: string;
+      };
+    };
+    createdAt: string;
+    slug: string;
+    title: string;
+    excerpt: string;
+    featuredImage: {
+      url: string;
+    };
+    categories: {
+      name: string;
+      slug: string;
+    }[];
+ 
+}
 
-export default function IndexPage() {
+
+
+export default async function IndexPage() {
+  const post  = await getPosteos()
   return (
+    
     <section className="container mx-auto px-10 mb-8 ">
       <div className="grid grid-cols-1 lg:grid-cols-12 gap-12">
         <div className="lg:col-span-8 col-span-1">
-          {post.map((post) => (
+          {post.map((post : Post) => (
             <PostCard key={post.title} post={post} />
           ))}
         </div>
@@ -29,3 +54,21 @@ export default function IndexPage() {
     </section>
   )
 }
+
+export async function getStaticProps() {
+  const posts = (await getPosts()) || [];
+  return {
+    props: { posts },
+  };
+}
+
+// `app` directory
+ 
+// This function can be named anything
+async function getPosteos() {
+  const res = await (await getPosts()) || [];
+  const post = res
+ 
+  return post
+}
+ 
