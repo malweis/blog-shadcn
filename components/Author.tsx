1582f66c32fd85482504111ef4e87da4bd077782
malweis <58945171+malweis@users.noreply.github.com>
2023-06-14 11:03:02 -0400

    feat: add author component

--- components/Author.tsx
+++ components/Author.tsx
@@ -1,5 +1,6 @@
 import React from 'react'
-
+import Image from 'next/image';
+import { grpahCMSImageLoader } from '@/utils';
 
 interface Author {
   
@@ -18,7 +19,21 @@ interface AuthorProps {
 
 const Author = ( {author} : AuthorProps) => {
   return (
-    <div>Author</div>
+    <div className="text-center mt-20 mb-8 p-12 relative rounded-lg bg-black bg-opacity-20">
+    <div className="absolute w-full flex justify-center right-0 left-0 -top-14">
+      <Image
+        unoptimized
+        loader={grpahCMSImageLoader}
+        alt={author.name}
+        height={100}
+        width={100}
+        className="align-middle rounded-full"
+        src={author.photo.url}
+      />
+    </div>
+    <h3 className="text-white mt-4 mb-4 text-xl font-bold">{author.name}</h3>
+    <p className="text-white text-ls">{author.bio}</p>
+  </div>
   )
 }
 
