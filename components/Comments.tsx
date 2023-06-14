7a60f07914dac6d3d93d4f6f4360ac3bdc8a6742
malweis <58945171+malweis@users.noreply.github.com>
2023-06-14 15:43:27 -0400

    feat: Añade componente de comentarios

--- components/Comments.tsx
+++ components/Comments.tsx
@@ -1,13 +1,56 @@
-import React from 'react'
-
+"use client"
+import React, {useState, useEffect} from 'react'
+import moment from 'moment';
+import parse from 'html-react-parser';
+import { getComments } from '@/services';
 interface Slug {
   slug: string;
 }
-
+  
+interface Comment {
+  createdAt: string;
+  name: string;
+  comment: string;
+}
+  
 
 const Comments = ({slug} :Slug) => {
+  const [comments, setComments] = useState([])
+
+  useEffect(() => {
+    getComments(slug).then((result) => setComments(result))
+  
+  
+  }, [])
+  
   return (
-    <div>Comments</div>
+    <>
+      {comments.length > 0 && (
+          <div className='bg-white shadow-lg rounded-lg p-8 pb-12 mb-8'>
+              <h3 className='text-xl mb-8 font-semibold border-b pb-4'>
+                {comments.length} 
+                {' '}
+                Comentarios
+              </h3>
+              {comments.map((comment : Comment) => (
+                <div key={comment.createdAt} className='border-b border-gray-100 mb-4 pb-4'>
+                      <p className='mb-4'>
+                        <span className='font-semibold'>
+                          {comment.name}
+                        </span>
+                        {' '}
+                        on
+                        {' '}
+                        {moment(comment.createdAt).format('MMMM Do YYYY')}
+                      </p>
+                      <p className='whitespace-pre-line text-gray-600 w-full'>
+                    {parse(comment.comment)}
+                      </p>
+                </div>
+              )) }
+          </div>
+      )}
+    </>
   )
 }
 
