d571ea20219976abe5938dfe354f98f17236a898
malweis <58945171+malweis@users.noreply.github.com>
2023-06-14 22:50:04 -0400

    feat: Soluciona tailwind repetidos
    vercel se queja y fuck it we ball

--- components/AdjacentPostCard.tsx
+++ components/AdjacentPostCard.tsx
@@ -53,7 +53,7 @@ const AdjacentPostCard = ({ post, position }: PostCardProps) => (
       <div className="arrow-btn absolute bottom-5 left-4 cursor-pointer rounded-full bg-pink-600 py-3 text-center">
         <svg
           xmlns="http://www.w3.org/2000/svg"
-          className="h-6 w-6 w-full text-white"
+          className="h-6 w-6 text-white"
           fill="none"
           viewBox="0 0 24 24"
           stroke="currentColor"
@@ -71,7 +71,7 @@ const AdjacentPostCard = ({ post, position }: PostCardProps) => (
       <div className="arrow-btn absolute bottom-5 right-4 cursor-pointer rounded-full bg-pink-600 py-3 text-center">
         <svg
           xmlns="http://www.w3.org/2000/svg"
-          className="h-6 w-6 w-full text-white"
+          className="h-6 w-6  text-white"
           fill="none"
           viewBox="0 0 24 24"
           stroke="currentColor"
